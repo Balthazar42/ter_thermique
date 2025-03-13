6d92264cf8256eae212606e7bc89884ae380e7e1
Balthazar2 <balthazar2.yt@gmail.com>
2025-03-13 19:42:29 +0100

    working on animation

--- graph.py
+++ graph.py
@@ -1,4 +1,7 @@
 import numpy as np
+from matplotlib.figure import Figure
+from matplotlib.axes import Axes
+from matplotlib.artist import Artist
 from matplotlib import pyplot as plt
 import matplotlib.animation as animation
 
@@ -7,12 +10,14 @@ class DiffusionGraph:
     """
     A graph modeling heat conduction in a network of capacitive bodies at uniform temperatures
     """
-    def __init__(self, n: int, C: np.ndarray, G: np.ndarray, pos: np.ndarray):
+    def __init__(self, n: int, C: np.ndarray, G: np.ndarray, pos: np.ndarray, fig: Figure=None, ax: Axes=None):
         """
         :param n: number of vertices
         :param C: capacitance of each vertex, n positive floats
         :param G: conductance of each edge, n * n positive floats, symmetric
         :param pos: visual position of each vertex, n * 2 floats
+        :param fig: matplotlib Figure on which to draw, will be created automatically if unspecified
+        :param ax: matplotlib Axes on which to draw, will be created automatically if unspecified
         """
         # Intrinsic properties
         self.n = n
@@ -33,8 +38,8 @@ class DiffusionGraph:
 
         # Plotting information
         self.pos = pos
-        self.fig, self.ax = None, None # Matplotlib figure and axes
-        self.anim = []
+        self.fig, self.ax = fig, ax # Matplotlib figure and axes
+        self.anim_artists = []
 
     def mean_T(self) -> float:
         """
@@ -50,32 +55,59 @@ class DiffusionGraph:
         """
         return np.sum(self.C * (self.T - self.mean_T()) ** 2) / np.sum(self.C)
 
-    def start_simulation(self):
+    def draw_vertices(self) -> Artist:
         """
-        Sets/resets the graph to its initial state
-        Create a new figure and clear the animation
+        Plot the vertices of the graph to this object's Axes
+        :return: Artist representing the plotted points
         """
+        scatter = self.ax.scatter(
+            x=self.pos[:, 0],
+            y=self.pos[:, 1],
+            s=self.C / np.max(self.C) * 50,
+            c=self.T,
+            marker="o",
+            cmap="plasma",
+            vmin=np.min(self.initial),
+            vmax=np.max(self.initial),
+            edgecolors=["w" if bc is None else "g" for bc in self.boundary_conditions],
+        )
+        return scatter
+
+    def draw_edges(self) -> list[Artist]:
+        pass
+
+    def animate(self, t_start: float=None, t_end: float=None, dt: float=None, initial: np.ndarray[float]=None):
+        self.t_start = t_start or self.t_start
+        self.t_end = t_end or self.t_end
+        self.dt = dt or self.dt
+        self.initial = initial or self.initial
+
         if self.t_start is None or self.t_end is None or self.dt is None or self.initial is None:
             raise RuntimeError("Missing simulation parameters")
+
         self.t = self.t_start
         self.T = self.initial
-        self.fig, self.ax = plt.subplots()
-        self.fig.tight_layout()
-        self.anim = []
+        if self.fig is None:
+            self.fig, self.ax = plt.subplots()
+            self.fig.tight_layout()
+            self.fig.set_facecolor("0.2")
+            self.ax.set_facecolor("0.2")
+            self.ax.set_axis_off()
+        self.anim_artists = []
 
-    def simulation_step(self):
-        """
-        Compute the next time step of the simulation
-        """
-        # Explicit Euler integration of the ODE
-        new_T = self.T + self.dt * self.A * self.T
+        while self.t < self.t_end:
+            # Draw the current state of the graph and add it to the animation
+            self.anim_artists.append(self.draw_edges())
+            self.anim_artists[-1].append(self.draw_vertices())
 
-        # Compute and apply boundary conditions
-        for i in range(self.n):
-            if self.boundary_conditions[i] is not None:
-                new_T[i] = self.boundary_conditions[i](self, i)
+            # Explicit Euler integration of the ODE
+            new_T = self.T + self.dt * self.A * self.T
 
-        # Apply changes
-        self.t += self.dt
-        self.T = new_T
+            # Compute boundary conditions if any
+            for i in range(self.n):
+                if self.boundary_conditions[i] is not None:
+                    new_T[i] = self.boundary_conditions[i](self, i)
 
+            # Apply changes
+            self.t += self.dt
+            self.T = new_T
