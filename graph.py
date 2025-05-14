d44c1f980a3e8414a7e4a74031c26c3227f816f9
Balthazar2 <balthazar2.yt@gmail.com>
2025-05-14 16:40:52 +0200

    renamed T as U, fixed atan, refactored plotting, added plot_temp

--- graph.py
+++ graph.py
@@ -25,7 +25,7 @@ class DiffusionGraph:
         self.n = n
         self.C = C
         self.G = G
-        # Matrix for the ODE system dT/dt = A * T
+        # Matrix for the ODE system dU/dt = A * U
         self.A = np.diag(1 / C) @ (G - np.diag(np.sum(G, axis=1)))
 
         # Simulation parameters and data
@@ -36,7 +36,7 @@ class DiffusionGraph:
         self.boundary_conditions: list[Callable[[DiffusionGraph, int], float] | None] = [None] * n
 
         self.t = 0              # Time
-        self.T = np.zeros(n)    # Temperatures of the nodes
+        self.U = np.zeros(n)    # Temperatures of the nodes
 
         # Plotting information
         if pos is not None:
@@ -45,106 +45,13 @@ class DiffusionGraph:
             self.pos = np.zeros((n, 2))
         self.fig, self.ax = fig, ax # Matplotlib figure and axes
 
-
-    def draw_vertices(self) -> Artist:
-        """
-        Plot the vertices of the graph to this object's Axes
-        :return: Artist representing the plotted points
-        """
-        scatter = self.ax.scatter(
-            x=self.pos[:, 0],
-            y=self.pos[:, 1],
-            s=self.C / np.max(self.C) * 500,
-            c=self.T,
-            marker="o",
-            cmap="plasma",
-            vmin=np.min(self.initial),
-            vmax=np.max(self.initial),
-            edgecolors=["w" if bc is None else "g" for bc in self.boundary_conditions],
-            linewidths=3,
-            zorder=np.inf,
-        )
-        return scatter
-
-    def draw_edges(self) -> list[Artist]:
-        """
-        Plot the edges of the graph to this object's Axes
-        :return: list of Artists representing the plotted lines
-        """
-        edges = []
-
-        for i in range(self.n):
-            for j in range(i):
-                if self.G[i, j] != 0:
-                    angle = np.pi / 2 + np.atan(3 * self.G[i, j] * (self.T[i] - self.T[j]) / np.max(self.G) / (np.max(self.initial) - np.min(self.initial)))
-                    edges.extend(self.ax.plot(
-                        (self.pos[i, 0], self.pos[j, 0]), # x
-                        (self.pos[i, 1], self.pos[j, 1]), # y
-                        color="w",
-                        linewidth=self.G[i, j] / np.max(self.G) * 2,
-                        path_effects=[
-                            TickedStroke(spacing=30, angle= angle * 180 / np.pi, length=0.2),
-                            TickedStroke(spacing=30, angle=-angle * 180 / np.pi, length=0.2),
-                            Normal(),
-                        ],
-                    ))
-
-        return edges
-
-    def animate(self, t_start: float=None, t_end: float=None, dt: float=None, initial: np.ndarray=None) -> ArtistAnimation:
-        """
-        Simulate the behaviour of the graph and render it to the figure as an animation
-
-        :param t_start: Simulation time start
-        :param t_end: Simulation time end
-        :param dt: Simulation time step
-        :param initial: Initial temperature conditions
-        :return: matplotlib ArtistAnimation, must be saved to a variable until show is called
-        """
-        self.t_start = t_start if t_start is not None else self.t_start
-        self.t_end = t_end if t_end is not None else self.t_end
-        self.dt = dt if dt is not None else self.dt
-        self.initial = initial if initial is not None else self.initial
-
-        if self.t_start is None or self.t_end is None or self.dt is None or self.initial is None:
-            raise RuntimeError("Missing simulation parameters")
-
-        self.t = self.t_start
-        self.T = self.initial
-        if self.fig is None:
-            self.fig, self.ax = plt.subplots()
-            self.fig.tight_layout()
-            self.fig.set_facecolor("0.2")
-            self.ax.set_facecolor("0.2")
-            self.ax.set_axis_off()
-
-        anim_artists = []
-        while self.t < self.t_end:
-            # Draw the current state of the graph and add it to the animation
-            anim_artists.append(self.draw_edges())
-            anim_artists[-1].append(self.draw_vertices())
-
-            # Explicit Euler integration of the ODE
-            new_T = self.T + self.dt * self.A @ self.T
-
-            # Compute boundary conditions if any
-            for i in range(self.n):
-                if self.boundary_conditions[i] is not None:
-                    new_T[i] = self.boundary_conditions[i](self, i)
-
-            # Apply changes
-            self.t += self.dt
-            self.T = new_T
-
-        return ArtistAnimation(fig=self.fig, artists=anim_artists, interval=50)
-
     def simulate(self, t_start: float=None, t_end: float=None, dt: float=None, initial: np.ndarray=None) -> tuple[np.ndarray, np.ndarray]:
         """
-        Simulate the behaviour of the graph and output the computed temperatures
-        Returns a tuple (t, T) where
-        t is a 1D array of the times for which the solution was computed
-        T is a 2D array of the temperatures of the vertices over time
-        T has the following shape : (number of vertices, number of time steps)
+        Simulate the behaviour of the graph and output the computed temperatures\n
+        Returns a tuple (t, U) where\n
+        t is a 1D array of the times for which the solution was computed\n
+        U is a 2D array of the temperatures of the vertices over time\n
+        U has the following shape : (number of vertices, number of time steps)\n
 
         :param t_start: Simulation time start
         :param t_end: Simulation time end
@@ -161,41 +68,140 @@ class DiffusionGraph:
             raise RuntimeError("Missing simulation parameters")
 
         t = np.arange(self.t_start, self.t_end, self.dt)
-        T = np.empty((self.n, len(t)))
-        T[:, 0] = self.initial
+        U = np.empty((self.n, len(t)))
+        U[:, 0] = self.initial
 
         self.t = self.t_start
-        self.T = self.initial
+        self.U = self.initial
         for j in range(1, len(t)):
             # Explicit Euler integration of the ODE
-            T[:, j] = self.T + self.dt * self.A @ self.T
+            U[:, j] = self.U + self.dt * self.A @ self.U
 
             # Compute boundary conditions if any
             for i in range(self.n):
                 if self.boundary_conditions[i] is not None:
-                    T[i, j] = self.boundary_conditions[i](self, i)
+                    U[i, j] = self.boundary_conditions[i](self, i)
 
             # Apply changes to the graph
             self.t = t[j]
-            self.T = T[:, j]
+            self.U = U[:, j]
 
-        return t, T
+        return t, U
 
-    def mean_temp(self, T: np.ndarray) -> np.ndarray:
+    def _draw_vertices(self, U: np.ndarray, Umin: float, Umax: float) -> Artist:
+        """
+        Plot the vertices of the graph to this object's Axes
+        :return: Artist representing the plotted points
+        """
+        scatter = self.ax.scatter(
+            x=self.pos[:, 0],
+            y=self.pos[:, 1],
+            s=self.C / np.max(self.C) * 500,
+            c=U,
+            marker="o",
+            cmap="plasma",
+            vmin=Umin,
+            vmax=Umax,
+            edgecolors=["w" if bc is None else "g" for bc in self.boundary_conditions],
+            linewidths=3,
+            zorder=np.inf,
+        )
+        return scatter
+
+    def _draw_edges(self, U: np.ndarray, Umin: float, Umax: float) -> list[Artist]:
+        """
+        Plot the edges of the graph to this object's Axes
+        :return: list of Artists representing the plotted lines
+        """
+        edges = []
+
+        for i in range(self.n):
+            for j in range(i):
+                if self.G[i, j] != 0:
+                    angle = np.pi / 2 + np.arctan(3 * self.G[i, j] * (U[i] - U[j]) / np.max(self.G) / (Umax - Umin))
+                    edges.extend(self.ax.plot(
+                        (self.pos[i, 0], self.pos[j, 0]), # x
+                        (self.pos[i, 1], self.pos[j, 1]), # y
+                        color="w",
+                        linewidth=self.G[i, j] / np.max(self.G) * 2,
+                        path_effects=[
+                            TickedStroke(spacing=30, angle= angle * 180 / np.pi, length=0.2),
+                            TickedStroke(spacing=30, angle=-angle * 180 / np.pi, length=0.2),
+                            Normal(),
+                        ],
+                    ))
+
+        return edges
+
+    def animate(self, t_start: float=None, t_end: float=None, dt: float=None, initial: np.ndarray=None) -> ArtistAnimation:
+        """
+        Simulate the behaviour of the graph and render it to the figure as an animation\n
+        The resulting animation must be saved to a variable until show is called\n
+        When running in a Jupyter notebook, requires %matplotlib notebook\n
+
+        :param t_start: Simulation time start
+        :param t_end: Simulation time end
+        :param dt: Simulation time step
+        :param initial: Initial temperature conditions
+        :return: matplotlib ArtistAnimation
+        """
+        # Compute temperatures with simulate
+        t, U = self.simulate(t_start, t_end, dt, initial)
+        Umin = np.min(U)
+        Umax = np.max(U)
+
+        # Create and configure a figure if there isn't already one
+        if self.fig is None:
+            self.fig, self.ax = plt.subplots()
+            self.fig.tight_layout()
+            self.fig.set_facecolor("0.2")
+            self.ax.set_facecolor("0.2")
+            self.ax.set_axis_off()
+
+        anim_artists = []
+        for j in range(len(t)):
+            # Draw the current state of the graph and add it to the animation
+            anim_artists.append(self._draw_edges(U[:, j], Umin, Umax))
+            anim_artists[-1].append(self._draw_vertices(U[:, j], Umin, Umax))
+
+        return ArtistAnimation(fig=self.fig, artists=anim_artists, interval=50)
+
+    def plot_temp(self, t_start: float=None, t_end: float=None, dt: float=None, initial: np.ndarray=None):
+        """
+        Simulate the behaviour of the graph and plot the curves of temperatures over time
+        Don't forget to call plt.show() after this
+
+        :param t_start: Simulation time start
+        :param t_end: Simulation time end
+        :param dt: Simulation time step
+        :param initial: Initial temperature conditions
+        """
+        # Compute temperatures with simulate
+        t, U = self.simulate(t_start, t_end, dt, initial)
+
+        # Create a figure if there isn't already one
+        if self.fig is None:
+            self.fig, self.ax = plt.subplots()
+        self.ax.set_xlabel("Temps")
+        self.ax.set_ylabel("Température")
+
+        self.ax.plot(t, U.T)
+
+    def mean_temp(self, U: np.ndarray) -> np.ndarray:
         """
         Weighted mean temperature of the graph,
-        given an array of temperatures as computed by DiffusionGraph.simulate
-        Constant over time if the system is isolated
+        given an array of temperatures as computed by DiffusionGraph.simulate\n
+        Constant over time if the system is isolated\n
         """
-        return self.C @ T / np.sum(self.C)
+        return self.C @ U / np.sum(self.C)
 
-    def msd_temp(self, T: np.ndarray) -> float:
+    def msd_temp(self, U: np.ndarray) -> float:
         """
         Weighted mean square deviation of the temperatures,
-        given an array of temperatures as computed by DiffusionGraph.simulate
-        Monotonically decreasing over time if the system is isolated
+        given an array of temperatures as computed by DiffusionGraph.simulate\n
+        Monotonically decreasing over time if the system is isolated\n
         """
-        return self.C @ (T - self.mean_temp(T)) ** 2 / np.sum(self.C)
+        return self.C @ (U - self.mean_temp(U)) ** 2 / np.sum(self.C)
 
 
 
@@ -239,9 +245,9 @@ gigraphe = DiffusionGraph(
 
 def line(n: int, cap: float=1, cond: float=1, period: float=None) -> DiffusionGraph:
     """
-    Create a line graph with uniform capacitance and conductance
-    Optionally, force the first vertex's temperature to follow
-    the function of time 10*sin(2pi/T)
+    Create a line graph with uniform capacitance and conductance\n
+    Optionally, force the first vertex's temperature to follow\n
+    the function of time 10*sin(2pi/period)\n
 
     :param n: Number of vertices
     :param cap: Capacitance of the vertices
@@ -271,7 +277,7 @@ def line(n: int, cap: float=1, cond: float=1, period: float=None) -> DiffusionGr
 
 def plane(p: int, q: int, cap: float=1, cond:float=1) -> DiffusionGraph:
     """
-    Create a square grid graph with uniform capacitance and conductance
+    Create a square grid graph with uniform capacitance and conductance\n
 
 
     :param p: Number of lines
@@ -309,15 +315,15 @@ if __name__ == "__main__":
     # g = plane(p, q)
     # # g.boundary_conditions[12] = lambda gr, i: 10 * np.sin(2*np.pi/10*gr.t)
     # initial = np.random.uniform(-10, 10, (p, q))
-    # t, T = g.simulate(
+    # t, U = g.simulate(
     #     t_start=0,
     #     t_end=15,
     #     dt=0.1,
     #     initial=initial.flatten(),
     # )
-    # print(t.shape, T.shape)
-    # print(t[0], T[:, 0])
-    # print(t[-1], T[:, -1])
+    # print(t.shape, U.shape)
+    # print(t[0], U[:, 0])
+    # print(t[-1], U[:, -1])
 
     g = line(5, 1, 1)
     print(g.A)
